1e7472d9adb7b05e54dc51c9f48ab4bacec7c093
Donald Doricent <ddoricent97@gmail.com>
2025-08-08 13:39:12 -0400

    adding tests to portfolio.

--- cypress/e2e/my-tests/hello-world.test.js
+++ cypress/e2e/my-tests/hello-world.test.js
@@ -1,9 +1,34 @@
 /// <reference types="cypress" />
 
-describe('Empty Test', () => {
+describe('Basic Tests', () => {
 
-    it('test one', () => {
+    it('Correct Header', () => {
         cy.visit('https://dondoricent.netlify.app')
+
+        // Mocha
+        cy.contains('Full-Stack Developer / QA Engineer').should('exist') // .should is an assertion
+
+        cy.contains('Contact Me')
+
+        //cy.get - used to target attributes
+
+        cy.get('div')
+
+        cy.get('#about').click()
+
+        cy.get('#projects').click()
+
+        //cy.should 
+
+        cy.contains('or want to collaborate?').should('exist')
+
+        // cy.viewport
+
+        cy.viewport(1280, 720) // can also check certain products i.e. iphone size or mac size
+
+        cy.viewport('ipad-mini')
+
+        
 
     })
 
