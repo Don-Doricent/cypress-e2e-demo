fd3d73c24fa83a28c42e6cfc40b0fad5628b9a96
Donald Doricent <ddoricent97@gmail.com>
2025-08-08 15:53:02 -0400

    slight update.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.test.js",
+    specPattern: "cypress/e2e/**/*.smoke.cy.js",
+    baseUrl: "https://dondoricent.netlify.app",
+
+     // Fixed viewport so recording is consistent
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
